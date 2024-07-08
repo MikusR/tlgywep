577a70e50b6307c01a6da01aedfa20b463d9a267
Mikus Rakis <mikus.rakis@gmail.com>
2024-07-08 13:00:19 +0000

    after db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_07_07_162435) do
-  create_table "names", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
