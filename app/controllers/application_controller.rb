f87636ca9e631d7fc56c7771d33b8c58fcfc06c3
Mikus Rakis <mikus.rakis@gmail.com>
2024-07-08 13:00:42 +0000

    if run on codespaces in dev skip csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+  if Rails.env.development?
+    skip_before_action :verify_authenticity_token 
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
